ab6d76b44f116d5c76c15c7d9384ba94a2cd3751
ChaseSinify <56804717+ChaseSinify@users.noreply.github.com>
2020-12-05 09:04:49 -0500

    updated 2020 day5

--- 2020/day6.py
+++ 2020/day6.py
@@ -10,10 +10,10 @@ from autoInput import getInput, startInputClock
 from autoSubmit import submitAnswer
 
 def main(lines):
-    binaries = [int(line.replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0"), 2) for line in lines]
-    minny, maxxy, realsum = min(binaries), max(binaries), sum(binaries)
-    return maxxy, ((maxxy + minny)*(maxxy - minny + 1)//2) - realsum # expected sum == (max + min)*(max - min + 1)/2
-    
+    print(lines)
+    part1, part2 = 0, 0
+    return part1, part2
+
 if __name__ == '__main__':
     year, day = os.path.basename(os.getcwd()), re.findall(r'^.*day(\d+).py$', __file__)[0]
     # getInput(year, day) #NOTE: COMMENT ONCE INPUT FETCHED
